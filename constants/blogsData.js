bf82e6fb99807466464ad5184cb7f2392bad922e
Alrian Yaseen <alrianvoice1@gmail.com>
2024-01-30 17:40:24 +0500

    blog page image functionality created

--- constants/blogsData.js
+++ constants/blogsData.js
@@ -5,6 +5,8 @@ export const blogsData = [
         date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
         imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
         blogName: 'How To Make a Paper Plane',
+        slug: 'how-to-make-a-paper-plane',
+    
         blogDesc: 'Explore the art of crafting a perfect paper plane with our step-by-step guide. Learn folding techniques, aerodynamics, and unleash your creativity. Embark on a journey of paper aviation mastery!'
     },
     {
@@ -12,6 +14,7 @@ export const blogsData = [
         imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
         date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
         blogName: 'The Future of Virtual Reality',
+        slug: 'The-Future-of-Virtual-Reality',
         blogDesc: 'Dive into the evolving landscape of virtual reality technology. Discover its applications, advancements, and the exciting possibilities that await in this immersive digital realm.'
     },
     {
@@ -19,6 +22,7 @@ export const blogsData = [
         imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
         date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
         blogName: 'Unraveling the Mysteries of Dark Matter',
+        slug: 'Unraveling-the-Mysteries-of-Dark-Matter',
         blogDesc: 'Join us on a scientific exploration as we delve into the enigmatic realm of dark matter. Uncover the current research, theories, and the quest to understand the invisible force shaping our universe.'
     },
     {
@@ -26,27 +30,6 @@ export const blogsData = [
         date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
         imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
         blogName: 'Hidden Gems: Off the Beaten Path Travel Destinations',
+        slug: 'Hidden-Gems',
         blogDesc: 'Escape the ordinary and discover lesser-known travel destinations. Immerse yourself in the charm of hidden gems, away from the tourist crowds. Your next adventure awaits!'
-    },
-    {
-        category: 'HEALTH',
-        date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
-        imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
-        blogName: 'Mindful Living: A Holistic Approach to Well-being',
-        blogDesc: 'Explore the principles of mindful living and cultivate a holistic approach to well-being. Learn practical tips, mindfulness exercises, and embrace a balanced lifestyle for a healthier you.'
-    },
-    {
-        category: 'ART',
-        imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
-        date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
-        blogName: 'Expressive Photography: Capturing Emotions Through the Lens',
-        blogDesc: 'Immerse yourself in the world of expressive photography. Discover techniques to capture emotions, tell compelling stories through your lens, and elevate your photography to new heights.'
-    },
-    {
-        category: 'FOOD',
-        date: `${dateNow.getDate()} / ${dateNow.getMonth()+1} / ${dateNow.getFullYear()}`,
-        imgSrc: "https://img.freepik.com/free-photo/dumbbells-floor-gym-ai-generative_123827-23744.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705708800&semt=ais",
-        blogName: 'Culinary Adventures: Exploring Global Flavors',
-        blogDesc: 'Embark on a culinary journey around the world. Delight your taste buds with diverse flavors, traditional recipes, and the rich cultural tapestry that defines global cuisine.'
-    }
-];
+    }]
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,33 +1,83 @@
-import React from 'react'
-import { blogsData } from '../constants/blogsData'
-import { FaArrowRight } from 'react-icons/fa'
-const blogs = () => {
-    return (
-        <div><section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="-my-8 divide-y-2 divide-gray-100">
-                    {
-                        blogsData.map((blogData, index) => (<div key={index} className="py-8 flex flex-wrap md:flex-nowrap hover:bg-gray-200 cursor-pointer">
-                            <div style={{backgroundImage: `url(${blogData.imgSrc})`}} className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { blogsData } from "../constants/blogsData";
+import { FaArrowRight } from "react-icons/fa";
+import { useRouter } from "next/router";
+
+const Blogs = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            {blogsData.map((blogData, index) => (
+              <Link
+                legacyBehavior
+                href={`/blogs/${blogData.slug.toLowerCase()}`}
+                key={index}
+              >
+                <article className="py-8 flex h-72 flex-wrap md:flex-nowrap hover:bg-gray-200 cursor-pointer">
+                  <div className="border-2 border-black overflow-hidden h-full">
+                  <img
+                  src={blogData.imgSrc}
+                    className="w-full h-full object-cover transition-transform transform hover:scale-[1.2] hover:opacity-85 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col  "
+                  > 
+                  </img>
+                  </div>
+                  <div className="md:flex-grow mx-4">
+                  <div className="flex flex-col"> 
+                  <span className="font-semibold text-sm title-font text-gray-700">
+                      {blogData.category}
+                    </span>
+                    <span className="mt-1 text-gray-500 text-sm">
+                      {blogData.date}
+                    </span></div>
                    
-                                <span className="font-semibold title-font text-gray-700">{blogData.category}</span>
-                                <span className="mt-1 text-gray-500 text-sm">{blogData.date}</span>
-                            </div>
-                            <div className="md:flex-grow">
-                                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">{blogData.blogName}</h2>
-                                <p className="leading-relaxed">{blogData.blogDesc}</p>
-                                <button className="text-black-500 p-2 rounded-md bg-red-200 hover:bg-red-400 inline-flex items-center mt-4">Learn More
-                                    <FaArrowRight className="w-4 h-4 ml-2" />
-                                </button>
-                            </div>
-                        </div>))
-                    }
+                    <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                      {blogData.blogName}
+                    </h2>
+                    <p className="leading-relaxed w-96">
+                      {blogData.blogDesc.length > 100
+                        ? `${blogData.blogDesc.substring(0, 150)}...`
+                        : blogData.blogDesc}
+                    </p>
+                    <div className="flex mt-2">
+                      <a
+                        className="rounded-lg"
+                        href="https://techypapa.com/author/claude-pearson/"
+                      >
+                        <img
+                          alt="Avatar photo"
+                          src="http://techypapa.com/wp-content/uploads/2023/12/cropped-claude-40x40.png"
+                          srcSet="http://techypapa.com/wp-content/uploads/2023/12/cropped-claude-80x80.png 2x"
+                          className="rounded-full"
+                          height="40"
+                          width="40"
+                          decoding="async"
+                        />
+                      </a>
+                      <div className="mx-3 font-sans">
+                        <div className="font-bold text-black text-md">
+                          <a href="https://techypapa.com/author/claude-pearson/">
+                            Claude Pearson
+                          </a>
+                        </div>
+                        <div className="-mt-1 text-sm">
+                          <div className="cs-meta-date">19 January 2024</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-
-                </div>
-            </div>
-        </section></div>
-    )
-}
-
-export default blogs
+export default Blogs;
